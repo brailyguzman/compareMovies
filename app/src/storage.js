cce5a1fa6cf61771a8a41abffa9e7725c903c735
Braily Guzman <brailyg@outlook.com>
2025-01-11 12:45:45 -0500

    Implement storage functions

---
+++ app/src/storage.js
@@ -0,0 +1,33 @@
+import initialMovies from './movie-data.json';
+import { storageKey } from './constants.js';
+
+export const setMovies = (value) => {
+    localStorage.setItem(storageKey, JSON.stringify(value));
+};
+
+export const getMovies = () => {
+    return JSON.parse(localStorage.getItem(storageKey));
+};
+
+export const resetMovies = () => {
+    setMovies(initialMovies);
+};
+
+export const initializeMovies = () => {
+    const storedMovies = getMovies();
+
+    if (!storedMovies) {
+        setMovies(initialMovies);
+        return initialMovies;
+    }
+
+    return storedMovies;
+};
+
+export const updateMovies = (newMovie) => {
+    const movies = getMovies();
+
+    movies.push(newMovie);
+
+    setMovies(movies);
+};
