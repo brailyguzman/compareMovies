cf8c3e939a4da066831c2315e54ef62af646b8a2
Braily Guzman <brailyg@outlook.com>
2025-01-11 12:46:08 -0500

    Implement renderMovie function

---
+++ app/src/render.js
@@ -0,0 +1,47 @@
+import { formatCurrency } from './constants.js';
+
+export const renderMovie = (movie, custom) => {
+    const { criticScore, audienceScore, domestic, genre, title } = movie;
+
+    const moviesUl = document.getElementById('movies-list');
+
+    const movieLi = document.createElement('li');
+
+    movieLi.classList.add('movie');
+
+    if (custom) {
+        movieLi.classList.add('custom-movie');
+    }
+
+    const movieTitle = document.createElement('h3');
+    movieTitle.classList.add('movie-title');
+    movieTitle.textContent = title;
+
+    const movieCritic = document.createElement('p');
+    movieCritic.classList.add('movie-critic');
+    movieCritic.textContent = `Critic Score: ${criticScore}%`;
+
+    const movieAudience = document.createElement('p');
+    movieAudience.classList.add('movie-audience');
+    movieAudience.textContent = `Audience Score: ${audienceScore}%`;
+
+    const movieDomestic = document.createElement('p');
+    movieDomestic.classList.add('movie-domestic');
+    movieDomestic.textContent = `Domestic Total: ${formatCurrency.format(
+        domestic
+    )}`;
+
+    const movieGenre = document.createElement('p');
+    movieGenre.classList.add('movie-genre');
+    movieGenre.textContent = `Genre: ${genre}`;
+
+    movieLi.append(
+        movieTitle,
+        movieCritic,
+        movieAudience,
+        movieDomestic,
+        movieGenre
+    );
+
+    moviesUl.appendChild(movieLi);
+};
